3819b0bd59a328623ee962c33708e4685f7077e1
İbrahim Yaşin <83788675+ledlyy@users.noreply.github.com>
2023-05-09 01:36:44 +0300

    Update questions.sql
    solution for 'operator precedence' questions added

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -8,9 +8,19 @@
 * that are from either Japan or Australia
 */
 
+select firstname, income, country from customers
+where ((age < 30 or age >= 50) and income > 50000) and (country = 'Japan' or country = 'Australia');
+
 /*
 * DB: Store
 * Table: Orders
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+
+select count(totalamount) from orders
+where totalamount > 100 and (orderdate >= '2004-06-01' and orderdate <= '2004-06-30' )
+
+-- both way can be usen for the answer
+-- EXTRACT(month from orderdate) = 6
+-- (orderdate >= '2004-06-01' AND orderdate <= '2004-06-30') 
