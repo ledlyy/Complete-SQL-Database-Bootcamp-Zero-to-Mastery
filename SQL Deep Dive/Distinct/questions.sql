1cc171c73ffd2872c3364648e998be6e4cfa50e7
İbrahim Yaşin <83788675+ledlyy@users.noreply.github.com>
2023-05-09 07:17:14 +0300

    Update questions.sql

--- SQL Deep Dive/Distinct/questions.sql
+++ SQL Deep Dive/Distinct/questions.sql
@@ -4,7 +4,7 @@
 * Question: What unique titles do we have?
 */
 
-SELECT * FROM titles;
+SELECT distinct title FROM titles;
 
 
 /*
@@ -13,7 +13,7 @@ SELECT * FROM titles;
 * Question: How many unique birth dates are there?
 */
 
-SELECT * FROM employees;
+SELECT count(distinct birth_date) FROM employees;
 
 /*
 * DB: World
@@ -22,5 +22,6 @@ SELECT * FROM employees;
 * Make sure there are no nulls
 */
 
-SELECT * FROM country;
+SELECT distinct lifeexpectancy FROM country
+where lifeexpectancy is not null;
 
