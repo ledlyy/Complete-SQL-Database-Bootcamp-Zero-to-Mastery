6a1417ee6cf0fef9fc4f7f0b024d71385fad9cd3
İbrahim Yaşin <83788675+ledlyy@users.noreply.github.com>
2023-05-10 04:41:16 +0300

    Update questions.sql

--- SQL Deep Dive/Views/questions.sql
+++ SQL Deep Dive/Views/questions.sql
@@ -4,8 +4,11 @@
 *  Database: Employees
 */
 
-CREATE VIEW "90-95" AS
--- ...
+CREATE VIEW "90-95" AS 
+select * 
+from employees
+where extract(year from hire_date) between 1990 and 1995
+
 
 /*
 *  Create a view "bigbucks" that:
@@ -13,5 +16,13 @@ CREATE VIEW "90-95" AS
 *  Database: Employees
 */
 
-CREATE VIEW "bigbucks" AS
--- ...
+create view "bigbucks" as
+select distinct e.emp_no, e.first_name,e.last_name, e.birth_date, e.hire_date, e.gender
+from employees as e
+inner join salaries as s using(emp_no)
+where s.salary > 80000
+order by emp_no
+
+
+
+
