b15c93c186bf03754dcced4ce87c48805532c592
İbrahim Yaşin <83788675+ledlyy@users.noreply.github.com>
2023-05-10 03:50:06 +0300

    Update questions.sql

--- SQL Deep Dive/NULLIF/questions.sql
+++ SQL Deep Dive/NULLIF/questions.sql
@@ -4,6 +4,14 @@
 * Question: Show NULL when the product is not on special (0)
 */
 
-SELECT *
-FROM products
+select *, CASE
+when special = 0 then nullif(1,1)
+else special 
+end
+ from products
+ 
+ -- or
+ 
+ select *, nullif(special, 0) from products
+
 
